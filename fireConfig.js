0ce19c9d2f94588110e4b7a5e33fa867ff47bf74
angelicasengson <163825004+angelicasengson@users.noreply.github.com>
2024-09-21 16:57:15 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyC8NnSMFiXgUJFeeYGJFsOHtXJ5pdi52kY",
+  authDomain: "sengson-crud.firebaseapp.com",
+  databaseURL: "https://sengson-crud-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "sengson-crud",
+  storageBucket: "sengson-crud.appspot.com",
+  messagingSenderId: "307697692526",
+  appId: "1:307697692526:web:16258370256ad3fdd31b7f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
